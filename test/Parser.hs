cbae866a13f9072c4908de358da2b1db96d6efd8
Vlad Dziuba <dzyubavlad@gmail.com>
2017-01-17 08:57:03 +0200

    Add simple testing for parser

---
+++ test/Parser.hs
@@ -0,0 +1,28 @@
+module Parser where
+
+import Control.Monad (replicateM)
+
+import JSTP.JSRS
+
+import Test.QuickCheck
+import Test.QuickCheck.Gen
+
+instance Arbitrary JValue where
+  arbitrary = sized $ \n -> oneof $ if n > 0
+  then recursive ++ elementary
+  else elementary
+    where bool       = fmap JBool arbitrary
+          null       = return JNull
+          undefined  = return JUndefined
+          string     = fmap JString arbitrary
+          number     = fmap JNumber arbitrary
+          elementary = [number, string, bool, undefined, null]
+          recursive  = [array, object]
+          array      = fmap JArray $ sized vector
+          object = sized $ \n -> do
+            let vectorGen :: Arbitrary a => Gen [a]
+                vectorGen =  arbitrary
+                        >>= (`replicateM` (resize (n - 1) arbitrary))
+            names  <- vectorGen
+            values <- vectorGen
+            return . JObj . fromList $ zip names values
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,9 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+import Test.QuickCheck
+import Parser
+
+import JSTP.JSRS
+import JSTP.Parser
+
+main = quickCheck
+     $ forAll (resize 2 arbitrary :: Gen JValue)
+     $ \x -> either (`collect` False) (property . (== x)) . parse $ show x
